27a080a98fac0eef080ff4a91b2add665e562596
fmajewsk <filip.majewski@nokia.com>
2024-07-08 11:43:52 +0200

    add sidebar route

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -23,7 +23,7 @@ const Sidebar = () => {
         <Tooltip>
           <TooltipTrigger asChild>
             <Link
-              href="#"
+              href="/mandaty"
               className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
             >
               <FileClock className="h-5 w-5" />
