5946026df910181599929a220388b7bed5b24786
majer565 <majewskifilip2k@gmail.com>
2024-08-03 14:24:19 +0200

    Repair table

--- src/components/data-table-implementation/TicketsTable.tsx
+++ src/components/data-table-implementation/TicketsTable.tsx
@@ -1,20 +1,56 @@
 "use client";
 
 // import { ticketFilters as columnFilters, ticketColumns as columns } from "@/lib/data-table-columns/ticket-columns";
-import { columns, columnFilters } from "../../lib/data-table-columns/test-columns";
-import { getCoreRowModel, useReactTable } from "@tanstack/react-table";
+import {
+  columns,
+  columnFilters,
+} from "../../lib/data-table-columns/test-columns";
+import {
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
 import { DataTable } from "../data-table/DataTable";
 import { MOCK_PAYMENTS } from "@/lib/data-table-columns/mock-payments";
+import { useSorting } from "../../hooks/useSorting";
+import { usePagination } from "../../hooks/usePagination";
+import { useColumnFilter } from "../../hooks/useColumnFilter";
 
 //Client side table does not work
 const TicketsTable = () => {
+  const { sorting, setSorting } = useSorting([]);
+  const { pagination, setPagination } = usePagination({
+    pageIndex: 0,
+    pageSize: 10,
+  });
+  const { filters, setFilters } = useColumnFilter([]);
+
   const table = useReactTable({
     data: MOCK_PAYMENTS,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onSortingChange: setSorting,
+    onPaginationChange: setPagination,
+    onColumnFiltersChange: setFilters,
+    state: {
+      sorting,
+      pagination,
+      columnFilters: filters,
+    },
   });
 
-  return <DataTable columnFilters={columnFilters} table={table} isDataLoading={false} />;
+  return (
+    <DataTable
+      columnFilters={columnFilters}
+      table={table}
+      isDataLoading={false}
+    />
+  );
 };
 
 export default TicketsTable;
--- src/hooks/useSorting.tsx
+++ src/hooks/useSorting.tsx
@@ -4,17 +4,19 @@ import { SortingState } from "@tanstack/react-table";
 import { useEffect, useState } from "react";
 import useSearchParamsUpdate from "./useSearchParamsUpdate";
 
-export const useSorting = (state: SortingState) => {
+export const useSorting = (state: SortingState, serverSide?: boolean) => {
   const [sorting, setSorting] = useState<SortingState>(state);
   const { updateSearchParams } = useSearchParamsUpdate();
 
   useEffect(() => {
-    const sort = sorting[0];
-    if (sort)
-      updateSearchParams([
-        { id: "sort", param: `${sort.id}.${sort.desc ? "desc" : "asc"}` },
-      ]);
-    else updateSearchParams([{ id: "sort", param: "" }]);
+    if (serverSide) {
+      const sort = sorting[0];
+      if (sort)
+        updateSearchParams([
+          { id: "sort", param: `${sort.id}.${sort.desc ? "desc" : "asc"}` },
+        ]);
+      else updateSearchParams([{ id: "sort", param: "" }]);
+    }
   }, [sorting]);
 
   return {
