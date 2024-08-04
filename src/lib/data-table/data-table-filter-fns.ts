af8885378b22fa237e8f0b0a2fd87597e15c4f98
majer565 <majewskifilip2k@gmail.com>
2024-08-04 16:38:26 +0200

    add datatable date type

---
+++ src/lib/data-table/data-table-filter-fns.ts
@@ -0,0 +1,66 @@
+import { Row } from "@tanstack/react-table";
+import { parse } from "date-fns";
+import { pl } from "date-fns/locale";
+import { resolveDateFromString } from "../utils";
+
+export function textFilterFn<TData>(
+  row: Row<TData>,
+  id: string,
+  filterValue: any
+) {
+  const rowValue = row.getValue(id) as string[];
+  const input = filterValue as string[];
+  if (rowValue.length > 0 && input[0]) {
+    return filterValue.some((value: string) => rowValue.includes(value));
+  }
+
+  return undefined;
+}
+
+export function rangeFilterFn<TData>(
+  row: Row<TData>,
+  id: string,
+  filterValue: any
+): any {
+  const rowValue = row.getValue(id) as number;
+  const input = filterValue as string[];
+  if (input.length === 2) {
+    const min = Number(input[0]) || 0;
+    const max = Number(input[1]) || 0;
+
+    return rowValue >= min && rowValue <= max;
+  }
+
+  return undefined;
+}
+
+export function selectFilterFn<TData>(
+  row: Row<TData>,
+  id: string,
+  filterValue: any
+): any {
+  const rowValue = row.getValue(id) as string;
+  const input = filterValue as string[];
+
+  if (input.length > 0) return input.some((value) => rowValue === value);
+  return undefined;
+}
+
+export function dateFilterFn<TData>(
+  row: Row<TData>,
+  id: string,
+  filterValue: any
+): any {
+  const rowValue = row.getValue(id) as string;
+  const input = filterValue as string[];
+
+  if (input.length === 2) {
+    const dateRange = resolveDateFromString(input, "LLL dd, y");
+    const value = parse(rowValue, "LLL dd, y", new Date(), { locale: pl });
+
+    if (!dateRange) return undefined;
+    return value >= dateRange.from! && value <= dateRange.to!;
+  }
+
+  return undefined;
+}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,8 @@
 import { ColumnFiltersState } from "@tanstack/react-table";
 import { type ClassValue, clsx } from "clsx";
+import { parse } from "date-fns";
+import { pl } from "date-fns/locale";
+import { DateRange } from "react-day-picker";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -24,3 +27,14 @@ export function filterToQuery(columnState: ColumnFiltersState): {
 
   return filters;
 }
+
+export const resolveDateFromString = (
+  value: string[],
+  format: string
+): DateRange | undefined => {
+  if (value.length !== 2) return undefined;
+  const from = parse(value[0], format, new Date(), { locale: pl });
+  const to = parse(value[1], format, new Date(), { locale: pl });
+
+  return { from, to };
+};
