572f7599c19b03ea33c962a0acfe3a5b6a382656
majer565 <majewskifilip2k@gmail.com>
2024-08-06 16:24:11 +0200

    add time and pdf

---
+++ src/components/dialog/ticket-file-dialog.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { FileSearch } from "lucide-react";
+import { Button } from "../ui/button";
+
+interface TicketFileDialogProps {
+  ticketNumber: string;
+  filePath: string;
+}
+
+const TicketFileDialog = ({ filePath, ticketNumber }: TicketFileDialogProps) => {
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <Button variant="ghost" size="icon">
+          <FileSearch className="h-4 w-4" />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="max-w-[70rem] border-border">
+        <DialogHeader>
+          <DialogTitle>{ticketNumber}</DialogTitle>
+          <DialogDescription>
+            <iframe  src={`/ticket_files/${filePath}`} className="w-full h-[48rem]" />
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default TicketFileDialog;
