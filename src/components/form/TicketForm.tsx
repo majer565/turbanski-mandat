dd3d7c45b5dd67335d7676c1a15bc13880d5a574
majer565 <majewskifilip2k@gmail.com>
2024-08-06 21:15:10 +0200

    add form

--- src/components/form/TicketForm.tsx
+++ src/components/form/TicketForm.tsx
@@ -1,9 +1,91 @@
-import React from 'react'
+"use client";
+
+import { ticketSchema } from "@/lib/form/ticket-schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "../ui/button";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
+import { Input } from "../ui/input";
 
 const TicketForm = () => {
-  return (
-    <div>TicketForm</div>
-  )
-}
+  const form = useForm<z.infer<typeof ticketSchema>>({
+    resolver: zodResolver(ticketSchema),
+  });
 
-export default TicketForm
+  const onSubmit = (values: z.infer<typeof ticketSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="grid grid-cols-2 gap-x-16 gap-y-8">
+          <FormField
+            control={form.control}
+            name="number"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>This is your public display name.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="number"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>This is your public display name.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="number"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>This is your public display name.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="number"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>This is your public display name.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="col-start-1 col-end-3 flex justify-center">
+            <Button type="submit" className="w-1/2">
+              Submit
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default TicketForm;
---
+++ src/lib/form/ticket-schema.ts
@@ -0,0 +1,15 @@
+"use client";
+
+import { z } from "zod";
+
+export const ticketSchema = z.object({
+  number: z.string().min(1).max(50),
+  date: z.string().min(2).max(50),
+  time: z.string().min(2).max(50),
+  vehiclePlateNumber: z.string().min(2).max(50),
+  amount: z.string().min(2).max(50),
+  currency: z.string().min(2).max(50),
+  postPayoutDate: z.string().min(2).max(50),
+  file: z.string().min(2).max(50),
+  driverId: z.string().min(2).max(50),
+});
