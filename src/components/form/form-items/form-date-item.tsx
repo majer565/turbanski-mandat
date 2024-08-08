e2809e11e77071e9e4c6476b8bfd731ab855157a
majer565 <majewskifilip2k@gmail.com>
2024-08-08 15:14:13 +0200

    add new form items

---
+++ src/components/form/form-items/form-date-item.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { FieldValues } from "react-hook-form";
+import { FormItemProps } from "./form-input-item";
+
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { CalendarIcon } from "lucide-react";
+import { Button } from "../../ui/button";
+
+interface FormDateProps<T extends FieldValues> extends FormItemProps<T> {
+  placeholder: string;
+}
+const FormSelectItem = <T extends FieldValues>(props: FormDateProps<T>) => {
+  return (
+    <FormField
+      control={props.form.control}
+      name="dob"
+      render={({ field }) => (
+        <FormItem className="flex flex-col">
+          <FormLabel>{props.label}</FormLabel>
+          <Popover>
+            <PopoverTrigger asChild>
+              <FormControl>
+                <Button
+                  variant={"outline"}
+                  className={cn(
+                    "w-[240px] pl-3 text-left font-normal",
+                    !field.value && "text-muted-foreground"
+                  )}
+                >
+                  {field.value ? (
+                    format(field.value, "PPP")
+                  ) : (
+                    <span>Wybierz datę</span>
+                  )}
+                  <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                </Button>
+              </FormControl>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0" align="start">
+              <Calendar
+                mode="single"
+                selected={field.value}
+                onSelect={field.onChange}
+                disabled={(date) =>
+                  date > new Date() || date < new Date("1900-01-01")
+                }
+                initialFocus
+              />
+            </PopoverContent>
+          </Popover>
+          {props.description && (
+            <FormDescription>{props.description}</FormDescription>
+          )}
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+export default FormSelectItem;
---
+++ src/components/form/form-items/form-file-item.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { FieldValues, Path, UseFormReturn } from "react-hook-form";
+import { Input } from "../../ui/input";
+
+export interface FormItemProps<T extends FieldValues> {
+  form: UseFormReturn<T>;
+  label: string;
+  name: string;
+  description?: string;
+}
+
+interface FormFileProps<T extends FieldValues> extends FormItemProps<T> {
+  placeholder: string;
+}
+
+const FormFileItem = <T extends FieldValues>(props: FormFileProps<T>) => {
+  return (
+    <FormField
+      control={props.form.control}
+      name={props.name as Path<T>}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{props.label}</FormLabel>
+          <FormControl>
+            <Input
+              id="picture"
+              type="file"
+              placeholder={props.placeholder}
+              {...field}
+            />
+          </FormControl>
+          {props.description && (
+            <FormDescription>{props.description}</FormDescription>
+          )}
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+export default FormFileItem;
